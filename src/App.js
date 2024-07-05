da459bdd6f4779512f1e0046e0f940170ccd1d43
Mathis Tremblay <mtremblay@nyx-hemera.com>
2024-07-05 14:44:50 -0400

    Making the styling work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,68 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { auth } from './components/firebase/firebase';
+import InfoPage from './components/Info/InfoPage';
+import RegisterPage from './components/Register/RegisterPage';
+import LoginPage from './components/Login/LoginPage';
+import { appConsts } from './appCfg/appConsts';
+import ProtectedRoute from './router/ProtectedRoute';
+import MainApp from './components/MainApp/MainApp';
+import ErrorPage from './router/ErrorPage';
+import HorairePage from './components/Horaire/HorairePage';
+import CostumePage from './components/Costume/CostumePage';
+import MessagesPage from './components/Messages/MessagesPage';
+import { AuthProvider } from './reactHooks/AuthContext';
 import './App.css';
-import LoginPage from "./components/Login/LoginPage";
-import {AuthProvider} from "./reactHooks/AuthContext";
-import {ToastContainer} from "react-toastify";
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+    const [user, setUser] = useState();
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            setUser(user);
+        });
+    });
     return (
-        <div>
-            <AuthProvider>
-                <LoginPage/>
-            </AuthProvider>
-            <ToastContainer/>
-        </div>
-    )
+        <Router>
+            <div className="App">
+                <div className="auth-wrapper">
+                    <div className="auth-inner">
+                        <Routes>
+                            <Route
+                                path="*"
+                                element={<ErrorPage/>}
+                            />
+                            <Route
+                                path="/"
+                                element={user ? <Navigate to={appConsts.routerPaths.home.info} /> : <LoginPage /> }
+                            />
+                            <Route path={appConsts.routerPaths.login} element={<LoginPage />}/>
+                            <Route path={appConsts.routerPaths.register} element={<RegisterPage />}/>
+                            <Route
+                                path={appConsts.routerPaths.home.base}
+                                element={user ? (
+                                    <AuthProvider>
+                                        <ProtectedRoute>
+                                            <MainApp/>
+                                        </ProtectedRoute>
+                                    </AuthProvider>)
+                                    : (<Navigate to={appConsts.routerPaths.login} />)}
+                            >
+                                <Route path={appConsts.routerPaths.home.info} element={<InfoPage />} />
+                                <Route path={appConsts.routerPaths.home.horaire} element={<HorairePage />} />
+                                <Route path={appConsts.routerPaths.home.costume} element={<CostumePage />} />
+                                <Route path={appConsts.routerPaths.home.messages} element={<MessagesPage />} />
+                            </Route>
+                        </Routes>
+                        <ToastContainer />
+                    </div>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
