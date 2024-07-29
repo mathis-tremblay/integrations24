2e92988c18f90a1ec584f2b7c8f52d6c72811a0e
Mathis Tremblay <mtremblay@nyx-hemera.com>
2024-07-29 10:59:34 -0400

    Info and horaire Dialog

--- src/components/Info/InfoDialog.js
+++ src/components/Info/InfoDialog.js
@@ -1,54 +1,66 @@
-import {Button} from "@mui/material";
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
 import "./InfoDialogStyle.css"
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import * as React from "react";
-import {useConfirm} from 'material-ui-confirm';
 
 export default function InfoDialog({ title, desc, text, icon }) {
-    const confirm = useConfirm();
+    const [openDialog, setOpenDialog] = React.useState(false);
 
     const handleClick = () => {
-        confirm({
-            title: title,
-            description: text,
-            confirmationText: "J'ai compris!",
-            hideCancelButton: true,
-            dialogProps: {
-                maxWidth: "lg",
-                fullWidth: true,
-                sx: {
+        setOpenDialog(true);
+    }
+
+    const handleClose = () => {
+        setOpenDialog(false);
+    }
+
+    return (
+        <div>
+            <Button variant={'contained'}
+                    size={'small'}
+                    color={'primary'}
+                    onClick={handleClick}
+                    style={{
+                        width: "80%",
+                        backgroundColor: 'rgba(85,136,38,0.85)',
+                        color: 'white',
+                        textTransform: 'none',
+                        margin: 30
+            }}
+            >
+                <div className="grid">
+                    <div className="icon">{icon}</div>
+                    <div className="title">{title}</div>
+                    <div className="desc">{desc}</div>
+                    <div className="arrow"><ChevronRightIcon/></div>
+                </div>
+            </Button>
+            <Dialog
+                open={openDialog}
+                onClose={handleClose}
+                maxWidth="lg"
+                fullWidth
+                style={{
                     '& .MuiDialog-paper': {
                         width: '70%',
                         maxHeight: '80vh',
-                    }},
-            }
-        })
-            .then(() => {
-                //Do nothing
-        })
-            .catch(() => {
-                //Do nothing
-            })
-    }
-    return (
-        <Button variant={'contained'}
-                size={'small'}
-                color={'primary'}
-                onClick={handleClick}
-                style={{
-                    width: "80%",
-                    backgroundColor: 'rgba(85,136,38,0.85)',
-                    color: 'white',
-                    textTransform: 'none',
-                    margin: 30
-        }}
-        >
-            <div className="grid">
-                <div className="icon">{icon}</div>
-                <div className="title">{title}</div>
-                <div className="desc">{desc}</div>
-                <div className="arrow"><ChevronRightIcon/></div>
-            </div>
-        </Button>
+                    }
+                }}
+            >
+                <DialogTitle>{title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {text}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color={"primary"} variant="contained"
+                            className="OkButton"
+                            >
+                        J'ai compris!
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     )
 }
